5bc07a30fadb73ffc9415f47d123a02903620118
Vanouphon Sirisouk <sirisoukvanouphon@gmail.com>
2020-08-31 07:31:14 -0400

    functions

---
+++ code/functions.py
@@ -0,0 +1,12 @@
+#defining a function
+def function():
+    print("this is my function")
+
+#defining a function with input parameters
+def function_para(inputs):
+    print("this is my function wiht a parameter of " + inputs)
+
+#returning information from functions
+def cube(num):
+    return (num * num * num)
+
