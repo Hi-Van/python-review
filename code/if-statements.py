ea2eb2a9f044f9a3aa46179c18bfb39a7d80ddea
Vanouphon Sirisouk <sirisoukvanouphon@gmail.com>
2020-08-31 07:44:04 -0400

    if statements

---
+++ code/if-statements.py
@@ -0,0 +1,41 @@
+#declaring if statements
+condition = True
+if condition:
+    print("conditon is true")
+else:
+    print("conditon is not true")
+
+#declaring or conditonals
+is_male = True
+is_tall = False
+
+if is_male or is_tall:
+    print("they are male or tall or both")
+else:
+    print("they are neither male nor tall")
+
+#declaring and conditionals
+is_smart = False
+is_short = True
+
+if is_smart and is_short:
+    print("you are smart and short")
+else:
+    print("you are not smart and short")
+
+#declaring not conditionals
+is_sick = False
+
+if not(is_sick):
+    print("you are not sick")
+else:
+    print("you are sick")
+
+#declaring comparisons
+num1 = 1
+num2 = 2
+
+if num1 > num2:
+    print("num1 is greater than num2")
+else:
+    print("num2 is greater than num1")
