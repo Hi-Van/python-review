955546c77aba2fcd61c2daf49feab179ba7d3c55
Vanouphon Sirisouk <sirisoukvanouphon@gmail.com>
2020-08-31 05:50:18 -0400

    move to code folder

---
+++ code/variables.py
@@ -0,0 +1,15 @@
+# with variables
+character_age = "18"
+character_name = "Van"
+
+print("There was once a man named " + character_name + ", he was " + character_age)
+
+# without variables
+
+print("There was once a man named Van, he was 18")
+
+# variable examples
+string = "this is a string"
+integer = 8
+float_example = 2.0
+boolean = True
